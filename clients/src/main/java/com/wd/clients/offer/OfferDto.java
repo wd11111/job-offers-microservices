ece85a0dc07fdc7e5aaa39e647cadf99404d12c7
Wojtek <xxwejtixx@gmail.com>
2023-01-31 20:44:33 +0100

    added validation messages properties

--- clients/src/main/java/com/wd/clients/offer/OfferDto.java
+++ clients/src/main/java/com/wd/clients/offer/OfferDto.java
@@ -1,14 +1,21 @@
 package com.wd.clients.offer;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-
-import java.io.Serializable;
 
 @Builder
 public record OfferDto(
+        @NotNull(message = "can not be null")
+        @NotBlank(message = "can not be empty")
         String title,
+        @NotNull(message = "can not be null")
+        @NotBlank(message = "can not be empty")
         String company,
+        @NotNull(message = "can not be null")
+        @NotBlank(message = "can not be empty")
         String salary,
-        String offerUrl)
-        implements Serializable {
+        @NotNull(message = "can not be null")
+        @NotBlank(message = "can not be empty")
+        String offerUrl) {
 }
